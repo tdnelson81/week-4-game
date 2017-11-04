09d495a23ac06e205b57a6d2835ca34219a6694e
Tom Nelson <tomnelson@Toms-MacBook-Pro.local>
2017-11-04 12:42:15 -0400

    2nd commit

---
+++ javascript/game.js
@@ -0,0 +1,78 @@
+
+	var wincounter = 0;
+ 	var lossCounter = 0;
+ 	var scoreCounter = 0;
+ 	var randomNum = Math.floor((Math.random() * 100) + 19);
+ 	
+ 	var crystalFour = Math.floor((Math.random() * 12) + 1);
+ 	var crystalThree = Math.floor((Math.random() * 12) + 1);
+ 	var crystalTwo = Math.floor((Math.random() * 12) + 1);
+ 	var crystalOne = Math.floor((Math.random() * 12) + 1);
+
+
+
+ 	var counters = function () {
+ 		$('#scoreCounter').empty();
+ 		$('#scoreCounter').append(scoreCounter);
+
+ 		$('#wincounter').empty();
+ 		$('#wincounter').append(wincounter);
+
+ 		$('#lossCounter').empty();
+ 		$('#lossCounter').append(lossCounter);
+ 	}
+
+
+ // restart game
+ 		var restart = function (){
+ 			scoreCounter = 0;
+ 			randomNum = Math.floor((Math.random() * 100) + 19);
+
+ 			$('#randomNum').empty();
+ 			$('#randomNum').append(randomNum);
+
+ 			var crystalFour = Math.floor((Math.random() * 12) + 1);
+ 			var crystalThree = Math.floor((Math.random() * 12) + 1);
+ 			var crystalTwo = Math.floor((Math.random() * 12) + 1);
+ 			var crystalOne = Math.floor((Math.random() * 12) + 1);
+ 			counters();
+ 		}	
+
+
+ 	var game = function (){
+ 		if (scoreCounter == randomNum) {
+			 wincounter = wincounter + 1;
+			 alert("You win!");
+ 			restart();
+ 		} else if (scoreCounter > randomNum) {
+ 			lossCounter = lossCounter + 1;
+ 			restart();
+ 		} else {
+ 			counters();
+ 		}
+ 	}
+
+ 	$('#randomNum').append(randomNum);
+ 	$('#scoreCounter').append(scoreCounter);
+
+ 	$( document ).ready(function() {
+ 		$('#crystalFour').click(function(){
+ 			scoreCounter = scoreCounter + crystalFour;
+ 			game();
+ 		})
+
+ 		$('#crystalThree').click(function(){
+ 			scoreCounter = scoreCounter + crystalThree;
+ 			game();
+ 		})
+
+ 		$('#crystalTwo').click(function(){
+ 			scoreCounter = scoreCounter + crystalTwo;
+ 			game();
+ 		})
+
+ 		$('#crystalOne').click(function(){
+ 			scoreCounter = scoreCounter + crystalOne;
+ 			game();
+ 		})
+ 	});
